1f64e4dc1c2071619831255fccc385bbd1ffda2c
Dan Cameron <dan@sproutapps.co>
2016-02-26 10:54:59 -0800

    Simplification and cleanup.
    Updates #1

--- controllers/panorama/pspsi-projects.php
+++ controllers/panorama/pspsi-projects.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * pspsi-projects.php 
+ * pspsi-projects.php
  * Functionallity on the Project Panorama side
  *
  * @package psp_sprout_invoices
@@ -15,11 +15,11 @@
  * @return 	(array) 		Modified array with additional meta_query arguements
  **/
 add_filter( 'psp_project_access_meta_query', 'pspsi_alter_access_meta_query', 10, 1 );
-function pspsi_alter_access_meta_query( $args ) { 
-		
-	$cuser				=	wp_get_current_user(); 
-	$client_meta		=	pspsi_client_dynamic_meta_query( pspsi_get_my_client_accounts( $cuser->ID ) );
-	$args 				=	array_merge( $args, $client_meta );
+function pspsi_alter_access_meta_query( $args ) {
+
+	$cuser				= wp_get_current_user();
+	$client_meta		= pspsi_client_dynamic_meta_query( pspsi_get_my_client_accounts( $cuser->ID ) );
+	$args 				= array_merge( $args, $client_meta );
 
 }
 
@@ -30,37 +30,22 @@ function pspsi_alter_access_meta_query( $args ) {
  * @param 	(int) $user_id 	wp_user ID
  * @return 	(array) 		sa_client post ID's
  **/
-function pspsi_get_my_client_accounts( $user_id = NULL ) { 
-	
+function pspsi_get_my_client_accounts( $user_id = null ) {
+
 	global $wpdb;
 
-	if( $user_id == NULL ) { 
-		
+	if ( $user_id == null ) {
+
 		$user		= wp_get_current_user();
 		$user_id	= $user->ID;
-		
-	}
-	
-	$query	= "SELECT $wpdb->posts.* FROM $wpdb->posts, $wpdb->postmeta WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id AND $wpdb->postmeta.meta_key = '_associated_users' AND $wpdb->postmeta.meta_value = " . $user_id . " AND $wpdb->posts.post_status = 'publish' AND $wpdb->posts.post_type = 'sa_client'";
 
-	$client_accounts 	= $wpdb->get_results( $query );
-	$client_account_ids	= array();
-	
-	if( !empty( $client_accounts ) ) { 
-	
-		foreach( $client_accounts as $account ) { 
-		
-			$client_account_ids[] = $account->ID;
-		
-		}
-		
-		return $client_account_ids;
-	
-	} else { 
-	
-		return FALSE;
-	
 	}
+
+	$client_ids = SI_Client::get_clients_by_user( $user_id );
+	if ( empty( $client_ids ) ) {
+		return false;
+	}
+	return $client_ids;
 
 }
 
@@ -71,30 +56,29 @@ function pspsi_get_my_client_accounts( $user_id = NULL ) {
  * @param 	(array)	$client_ids	Array of sa_client post IDs
  * @return 	(array)
  **/
-function pspsi_client_dynamic_meta_query( $client_ids = NULL ) { 
-	
-	if( $client_ids == NULL ) { 
-	
+function pspsi_client_dynamic_meta_query( $client_ids = null ) {
+
+	if ( $client_ids == null ) {
+
 		$client_ids = pspsi_get_my_client_accounts();
-	
+
 	}
-	
+
 	$meta_query = array();
-	
-	if( !empty( $client_ids ) ) { 
-	
-		foreach( $client_ids as $client_id ) { 
-	
+
+	if ( ! empty( $client_ids ) ) {
+
+		foreach ( $client_ids as $client_id ) {
+
 			$meta_query[] = array(
-				'key'		=>	'pspsi_clients',
-				'value'		=>	$client_id,
-				'compare'	=>	'LIKE'
+				'key'		=> 'pspsi_clients',
+				'value'		=> $client_id,
+				'compare'	=> 'LIKE',
 			);
-	
+
 		}
-		
 	}
-	
+
 	return $meta_query;
 
 }
@@ -108,29 +92,27 @@ function pspsi_client_dynamic_meta_query( $client_ids = NULL ) {
  * @return 	(boolean)
  **/
 add_filter( 'panorama_check_access', 'pspsi_check_access_on_post', 10, 2 );
-function pspsi_check_access_on_post( $result, $post_id ) { 
+function pspsi_check_access_on_post( $result, $post_id ) {
 
 	$cuser		= wp_get_current_user();
 	$accounts 	= pspsi_get_my_client_accounts( $cuser->ID );
-	
+
 	// If the result is false, the user is assigned to clients and the project has clients
-	
-	if( ( $result == FALSE ) && ( !empty( $accounts ) ) && ( get_field( 'pspsi_clients', $post_id ) ) ) { 
-	
-		$clients = get_field( 'pspsi_clients' );	
-		
-		foreach( $accounts as $account ) { 
-		
-			if( in_array( $account, $clients ) ) { 
-			
-				$result = TRUE;
-			
+
+	if ( ( $result == false ) && ( ! empty( $accounts ) ) && ( get_field( 'pspsi_clients', $post_id ) ) ) {
+
+		$clients = get_field( 'pspsi_clients' );
+
+		foreach ( $accounts as $account ) {
+
+			if ( in_array( $account, $clients ) ) {
+
+				$result = true;
+
 			}
-		
 		}
-	
 	}
-	
+
 	return $result;
 
 }
