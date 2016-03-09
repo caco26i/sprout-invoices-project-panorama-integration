443cfed6913a6352d316ead6f698ed7c035231ed
Dan Cameron <dan@sproutapps.co>
2016-03-08 16:16:48 -0800

    Reorganization

---
+++ load.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * init.php
+ * Initalize the plugin and load any dependencies
+ *
+ * @package psp_sprout_invoices
+ * @since 1.0
+ */
+
+function pspsi_load() {
+
+	require_once PSPSI_DIR . '/controllers/PSP_SI.php';
+	require_once PSPSI_DIR . '/controllers/Project_Panorama_Access.php';
+	require_once PSPSI_DIR . '/controllers/Project_Panorama_Admin.php';
+	require_once PSPSI_DIR . '/controllers/Project_Panorama_View.php';
+
+	do_action( 'pspsi_load_classes' );
+
+	PSPSI_Project_Panorama_Access::init();
+	PSPSI_Project_Panorama_Admin::init();
+	PSPSI_Project_Panorama_View::init();
+
+	do_action( 'pspsi_loaded' );
+
+}
+
