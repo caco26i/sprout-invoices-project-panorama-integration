a96bc4ca7a9a92430ab73e852452d55b4dcc478b
Ross Johnson <ross@3.7designs.co>
2016-06-17 19:18:03 -0400

    Adds check to ensure both required plugins are installed. Better responsive tables

--- load.php
+++ load.php
@@ -9,20 +9,35 @@
 
 function pspsi_load() {
 
-	require_once PSPSI_DIR . '/controllers/PSP_SI.php';
-	require_once PSPSI_DIR . '/controllers/Project_Panorama_Access.php';
-	require_once PSPSI_DIR . '/controllers/Project_Panorama_Admin.php';
-	require_once PSPSI_DIR . '/controllers/Project_Panorama_View.php';
-	require_once PSPSI_DIR . '/controllers/Sprout_Invoices_Admin.php';
+	if( ( class_exists( 'SI_Project' ) ) && ( function_exists( 'psp_initalize_application' ) ) ) {
 
-	do_action( 'pspsi_load_classes' );
+		require_once PSPSI_DIR . '/controllers/PSP_SI.php';
+		require_once PSPSI_DIR . '/controllers/Project_Panorama_Access.php';
+		require_once PSPSI_DIR . '/controllers/Project_Panorama_Admin.php';
+		require_once PSPSI_DIR . '/controllers/Project_Panorama_View.php';
+		require_once PSPSI_DIR . '/controllers/Sprout_Invoices_Admin.php';
 
-	PSPSI_Project_Panorama_Access::init();
-	PSPSI_Project_Panorama_Admin::init();
-	PSPSI_Project_Panorama_View::init();
-	PSPSI_Sprout_Invoices_Admin::init();
+		do_action( 'pspsi_load_classes' );
 
-	do_action( 'pspsi_loaded' );
+		PSPSI_Project_Panorama_Access::init();
+		PSPSI_Project_Panorama_Admin::init();
+		PSPSI_Project_Panorama_View::init();
+		PSPSI_Sprout_Invoices_Admin::init();
+
+		do_action( 'pspsi_loaded' );
+
+	} else {
+
+		add_action( 'admin_notices', 'pspsi_missing_plugins_notice' );
+
+	}
 
 }
 
+function pspsi_missing_plugins_notice() { ?>
+
+	<div id="message" class="error notice is-dismissable">
+		<p>The <strong>Sprout Invoices &amp; Panorama Integration</strong> requires both <a href="http://www.sproutapps.com" target="_new">Sprout Invoices</a> and <a href="http://www.projectpanorama.com" target="_new">Project Panorama</a>.</p>
+	</div>
+
+<?php }
